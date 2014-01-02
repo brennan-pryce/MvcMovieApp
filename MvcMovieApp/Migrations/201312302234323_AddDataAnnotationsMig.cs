796ed22a0120cba955a4a325aebf48254c8af002
Brennan Pryce <bpryce@problemsolutions.net>
2014-01-02 18:22:06 -0500

    Commit all the things

---
+++ MvcMovieApp/Migrations/201312302234323_AddDataAnnotationsMig.cs
@@ -0,0 +1,22 @@
+namespace MvcMovieApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDataAnnotationsMig : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "Title", c => c.String(nullable: false));
+            AlterColumn("dbo.Movies", "Genre", c => c.String(nullable: false));
+            AlterColumn("dbo.Movies", "Rating", c => c.String(maxLength: 13));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Rating", c => c.String());
+            AlterColumn("dbo.Movies", "Genre", c => c.String());
+            AlterColumn("dbo.Movies", "Title", c => c.String());
+        }
+    }
+}
---
+++ MvcMovieApp/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovieApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+        
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [StringLength(13)]
+        public string Rating { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
